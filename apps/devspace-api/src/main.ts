7acfb87a08460673e00820d24f0acde4b297a6b8
Anton Wallstedt <antonwallstedt@outlook.com>
2025-06-11 17:21:35 +0200

    fix(devspace-api): handle Vercel edge functions

--- apps/devspace-api/src/main.ts
+++ apps/devspace-api/src/main.ts
@@ -1,23 +1,47 @@
 import Fastify from 'fastify';
 import { app } from './app/app';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3001;
-
-// Instantiate Fastify with some config
 const server = Fastify({
   logger: true,
 });
 
-// Register your application as a normal plugin.
 server.register(app);
 
-// Start listening.
-server.listen({ port, host }, (err) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  } else {
-    console.log(`[ ready ] http://${host}:${port}`);
+if (process.env.NODE_ENV !== 'production') {
+  const host = process.env.HOST ?? 'localhost';
+  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+
+  const start = async () => {
+    try {
+      await server.listen({ port, host });
+      console.log(`Server is running at http://${host}:${port}`);
+    } catch (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+  };
+  start();
+}
+
+export default async function handler(req: any, res: any) {
+  try {
+    await server.ready();
+    const response = await server.inject({
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      body: req.body,
+    });
+
+    for (const [key, value] of Object.entries(response.headers)) {
+      res.setHeader(key, value);
+    }
+
+    res.statusCode = response.statusCode;
+    res.end(response.payload);
+  } catch (error) {
+    console.error('Error handling request:', error);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
   }
-});
+}
